bee904a8a15d6220dcbf22b379d3a617de5c1879
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-08-18 23:51:35 +0530

    Learning java exception handling

---
+++ JAVA/MySimpleProject/src/main/App.java
@@ -0,0 +1,10 @@
+
+
+
+// Removed import for non-existent package
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ JAVA/MySimpleProject/src/utils/Greeter.java
@@ -0,0 +1,14 @@
+
+package utils;
+
+public class Greeter {
+  public void sayHello() {
+    System.out.println("Hello from the utils package!");
+  }
+}
+
+public class Greeter {
+    public void sayHello() {
+        System.out.println("Hello from the utils package!");
+    }
+}
