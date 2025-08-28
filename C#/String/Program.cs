b381d281f83f66cfa898c8dab1ed61a1b1ec5bfd
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-08-28 22:46:50 +0530

    learning about html forms and tables

---
+++ C#/String/Program.cs
@@ -0,0 +1,46 @@
+﻿// Number input
+// int n1, n2;
+// Console.Write("Enter First Number : "); 
+// string s = Console.ReadLine();
+// n1 = int.Parse(s);
+// Console.Write("Enter Second Number : "); 
+// s = Console.ReadLine();
+// n2 = int.Parse(s);
+
+// int sum = n1 + n2;
+// Console.WriteLine("The sum is : " + sum);
+
+
+// s = sum.ToString();
+
+
+//Character input
+
+// char alphabet1;
+// char alphabet2;
+
+// Console.Write("Enter first alphabet : ");
+// string s2 = Console.ReadLine();
+// char c = char.Parse(s2);
+
+// int n = c;
+
+// Console.WriteLine(n); 
+
+
+// Console.Write("Enter Number : ");
+// string s = Console.ReadLine();
+// int n = int.Parse(s);
+
+// char c = (char) n; 
+
+// Console.WriteLine(c); 
+
+
+int n1 = 10;
+int n2 = 10;
+
+Console.WriteLine("The Sum of " + n1 + "and " + n2 + " is " + (n1 + n2));
+
+
+
