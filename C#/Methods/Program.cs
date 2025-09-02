0c17e0686a92fe3477c3dffc7067b21e146e9a6b
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-09-02 20:55:48 +0530

    Learning About Method Overloading

---
+++ C#/Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+
+
+
+  public static void Main()
+  {
+    Console.WriteLine("Hi there");
+    string fullName = Add("Faizan", "Shaikh");
+    Console.WriteLine(fullName);
+    
+  }
+  public static string Add(string s1, string s2)
+  {
+    return s1 + " " + s2;
+  }
+
+
+}
