ce4667f4c6ae02fced1b2c313f37935b19837c1b
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-09-07 17:53:28 +0530

    Learning about passed by value and passed by references

---
+++ C#/Pass By Value & Reference/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    int a, b, c, d;
+    a = b = c = d = 10;
+    Foo(a, out b, ref c, in d);
+    Console.WriteLine($"{a} {b} {c} {d}");
+
+  }
+
+  static void Foo(int n1, out int n2, ref int n3, in int n4)
+  {
+    n2 = 10; 
+  }
+  
+}
