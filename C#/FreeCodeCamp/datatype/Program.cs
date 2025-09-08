754a21ae003a2f0d53a2c5e5b20884766d4b3c4f
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-09-08 22:31:14 +0530

    Learning datatype

---
+++ C#/FreeCodeCamp/datatype/Program.cs
@@ -0,0 +1,56 @@
+﻿// int[] data = new int[3];
+
+// int[] A= new int[1];
+// A[0] = 2;
+
+// int[] B = A;
+// B[0] = 5;
+
+// Console.WriteLine("--Reference Types--");
+// Console.WriteLine($"A[0]: {A[0]}");
+// Console.WriteLine($"B[0]: {B[0]}");
+
+// int[] array1 = new int[1];
+// array1[0] = 50;
+// int[] array2 = array1;
+// array2[0] = 100;
+
+// Console.WriteLine("array 1 : " + array1[0]);
+// Console.WriteLine("array 2 : " + array2[0]);
+
+// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+string name = "Bob";
+Console.WriteLine(int.Parse(name));
+
