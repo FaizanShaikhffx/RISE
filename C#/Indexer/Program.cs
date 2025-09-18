af943daa22b84243c3a0da35f8d924c72c725556
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-09-18 16:15:26 +0530

    Assignment 4

---
+++ C#/Indexer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Parent
+{
+  private string[] items = new string[3];
+
+  public virtual string this[int index]
+  {
+    get {
+      Console.WriteLine("Parent");
+      return items[index]; }
+    set
+    {
+      items[index] = value;
+    }
+  }
+
+}
+
+class Child : Parent
+{ 
+  public override string this[int index]
+  {
+    get {
+      Console.WriteLine("Child");
+       return "Dericed : " + base[index];
+    }
+    set
+    {
+      base[index] = value;
+    }
+  }
+
+}
+
+class Program
+{
+
+  static void Main(string[] args)
+  {
+    Parent p1 = new Parent();
+    // Parent p1 = new Child();
+    p1[0] = "Faizan";
+    Console.WriteLine(p1[0]);
+  }
+
+}
---
+++ Web Development/JavaScript/Assignment 4/script.js
@@ -0,0 +1,145 @@
+
+let DolphinScore = [96, 108, 89];
+let KoalasScore = [88, 91, 110];
+
+let averageDolphinScore = 0; 
+let averageKoalasScore = 0;
+
+let sum = 0; 
+let sum2 = 0; 
+
+function calculateDolphinsScore(DolphinScore){
+
+  for(let i = 0; i<DolphinScore.length; i++){
+    sum = sum + DolphinScore[i]; 
+  }
+
+  averageDolphinScore = sum / DolphinScore.length; 
+  console.log(averageDolphinScore)
+}
+
+
+function calculateKoalaScore(KoalasScore){
+
+
+  for(let i = 0; i<KoalasScore.length; i++){
+    sum2 = sum2 + KoalasScore[i];
+  }
+  
+  averageKoalasScore = sum2/ KoalasScore.length;
+
+  console.log(averageKoalasScore);
+
+}
+
+function checkHighScore(averageDolphinScore, averageKoalasScore){
+  if(averageDolphinScore > averageKoalasScore){
+    console.log("The winner is Dolphins with "+averageDolphinScore+" score");
+  }else if(averageDolphinScore > averageKoalasScore){
+    console.log("The winner is Koalas with "+averageKoalasScore+" score");
+  }else if(averageDolphinScore == averageKoalasScore){
+    console.log("The match is draw");
+  }
+}
+
+
+calculateDolphinsScore(DolphinScore);
+
+
+
+let result =  calculateDolphinsScore(DolphinScore);
+console.log(`Dolphins Score : ${result}`);
+console.log("Koalas Score :"+calculateKoalaScore(KoalasScore));
+
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
