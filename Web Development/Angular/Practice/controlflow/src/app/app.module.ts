a86d818e7ec76f3f645d12248d69473fea1cfd58
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-09-29 10:19:06 +0530

    Learning Basic Routing

--- Web Development/Angular/Practice/controlflow/src/app/app.module.ts
+++ Web Development/Angular/Practice/controlflow/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SwitchCaseComponent } from './switch-case/switch-case.component';
 import { LoopsComponent } from './loops/loops.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwitchCaseComponent,
-    LoopsComponent
+    LoopsComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
