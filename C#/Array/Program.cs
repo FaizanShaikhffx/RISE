fe615333bf7f06865b985382494318a4758c969f
FaizanShaikhffx <iamfaizzan@gmail.com>
2025-10-01 22:38:46 +0530

    Learning about tuples in c# and practicing javascript

--- C#/Array/Program.cs
+++ C#/Array/Program.cs
@@ -11,23 +11,59 @@
 // object ob = new object[5] { 1, true, 'A', 10d, false };
 
 
-Console.Write("Enter the NUmber Of Element : ");
-int n = int.Parse(Console.ReadLine());
-int[] arr = new int[n];
-for (int i = 0; i < arr.Length; i++) {
-  Console.Write("Enter a Number : ");
-  arr[i] = int.Parse(Console.ReadLine());  
-}
-int sum = 0;
-for (int i = 0; i < arr.Length; i++)
-{
-  sum = sum + arr[i];
-  // Console.Write(arr[i] + " ");
-}
-Console.WriteLine("Sum is : " + sum); 
-Console.WriteLine("Average is : " +(1.0 * sum / n));
+// Console.Write("Enter the NUmber Of Element : ");
+// int n = int.Parse(Console.ReadLine());
+// int[] arr = new int[n];
+// for (int i = 0; i < arr.Length; i++) {
+//   Console.Write("Enter a Number : ");
+//   arr[i] = int.Parse(Console.ReadLine());  
+// }
+// int sum = 0;
+// for (int i = 0; i < arr.Length; i++)
+// {
+//   sum = sum + arr[i];
+//   // Console.Write(arr[i] + " ");
+// }
+// Console.WriteLine("Sum is : " + sum); 
+// Console.WriteLine("Average is : " +(1.0 * sum / n));
 
-for (int i = arr.Length - 1; i >= 0; i--)
-{
-  Console.WriteLine(arr[i] + " "); 
-}
+// for (int i = arr.Length - 1; i >= 0; i--)
+// {
+//   Console.WriteLine(arr[i] + " "); 
+// }
+
+int[] arr = new int[5] { 5, 2, 3, 4, 5 };
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//   Console.WriteLine(arr[i]);
+// }
+
+// foreach (int i in arr)
+// {
+//   Console.WriteLine(i);
+// }
+
+// Console.WriteLine(arr.Sum()); 
+// Console.WriteLine(arr.Average()); 
+// Console.WriteLine(arr.Max()); 
+// Console.WriteLine(arr.Min()); 
+
+// Array.Reverse(arr);
+// foreach (var i in arr)
+// {
+//   Console.WriteLine(i); 
+// }
+
+// int succ = Array.BinarySearch(arr, 3);
+// Console.WriteLine(succ);
+
+
+
+int[,] array2Dimention = new int[3, 2] {
+  {1, 2 },
+  {3, 4 },
+  {5, 6 }
+ };
+
+Console.Write(array2Dimention[0, 0]);
---
+++ C#/Tuples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+
+  public static (int sum, int product) Calculate(int a, int b)
+  {
+    return ((a + b), (a * b));
+  }
+
+  public static void Main(string[] args)
+  {
+    //Old-Syntax
+    // Tuple<int, string> student = new Tuple<int, string>(1, "Faizan");
+    // Console.WriteLine(student.Item1);
+
+    // var name = Tuple.Create(1, "Faizan");
+    // Console.Write(name.Item1);
+
+    // var student = (id: 1, name: "Faizan");
+    // Console.Write(student.id); 
+
+
+    // var d = Calculate(10, 20);
+    // Console.Write(d);
+
+    var (name, age, rollNumber) = ("Faizan", 23, 1);
+    Console.Write(name); 
+
+
+
+  }
+
+
+}
